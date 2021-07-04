1c1b1319a90f09d3076e6bfcc1f5c7e62dace941
skyrocketted <citricthunderous@gmail.com>
2021-07-04 19:36:49 +0100

    💥💄 Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="min-h-screen object-fill bg-gradient-to-r from-red-500 to-blue-900 flex items-center justify-center flex-col">
+        <figure className="rounded-lg ring-4 ring-gray-300 bg-gray-50 w-full max-w-xs m-2">
+          <img src="https://avatars.githubusercontent.com/u/78935393" 
+            width="260"
+            height="260"
+            className="animate-wiggle mx-auto rounded-full outline-none hover:ring-4 hover:ring-pink-500 hover:ring-opacity-50 mt-2"
+            alt=""
+          />
+          <h3 className="text-center text-bold p-6">
+            Hello!
+            <br />
+            I am Sky, better known as Skyrocket(ted) or Sky🚀.
+            <br />
+            I'm a developer from the Central London area in the UK.
+          </h3>
+        </figure>
+      </div>
     </div>
   );
 }
